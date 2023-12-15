aef05c2ddeb23a7918e127b80663f03830c00748
youjuny <youyoung2774@gmail.com>
2023-12-15 16:22:34 +0900

    first

---
+++ src/main/java/com/korea/project2_team4/Project2Team4Application.java
@@ -0,0 +1,13 @@
+package com.korea.project2_team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Team4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/korea/project2_team4/Project2Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.project2_team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

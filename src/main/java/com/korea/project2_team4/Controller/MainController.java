05e47fd9225867bb64b2e38d917126b84ed04579
youjuny <youyoung2774@gmail.com>
2023-12-18 14:23:52 +0900

    team3

---
+++ src/main/java/com/korea/project2_team4/Controller/MainController.java
@@ -0,0 +1,14 @@
+package com.korea.project2_team4.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "main_form";
+    }
+}
--- src/main/java/com/korea/project2_team4/Controller/MemberController.java
+++ src/main/java/com/korea/project2_team4/Controller/MemberController.java
@@ -1,26 +1,44 @@
 package com.korea.project2_team4.Controller;
 
 import com.korea.project2_team4.Model.Form.MemberCreateForm;
+import com.korea.project2_team4.Service.MemberService;
+import jakarta.validation.Valid;
 import lombok.Builder;
+import lombok.Getter;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
 
 @Controller
 @RequestMapping("/member")
+@Builder
 public class MemberController {
 
+    private final MemberService memberService;
 
+
+    @GetMapping("/signup")
     public String signup(MemberCreateForm memberCreateForm) {
 
-        return "signup_form";
+        return "Member/signup_form";
     }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberCreateForm memberCreateForm, BindingResult bindingResult) {
+
+
+
+        memberService.create(memberCreateForm);
+
+
+
+        return "redirect:/";
+    }
+
 
 
 }

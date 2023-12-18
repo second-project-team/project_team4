cf0fc296e5f4742e2798609e9b0e13f6352dbb91
youjuny <youyoung2774@gmail.com>
2023-12-18 15:02:03 +0900

    login Service

---
+++ src/main/java/com/korea/project2_team4/Config/UserRole.java
@@ -0,0 +1,15 @@
+package com.korea.project2_team4.Config;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    UserRole(String value) {
+        this.value = value;
+    }
+
+    private final String value;
+}

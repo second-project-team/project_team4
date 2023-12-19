ea33131b22591c649f04bfd5d90f1d055b061fe2
새로ìpogeum <youyoung2774@gmail.com>
2023-12-19 17:21:51 +0900

    프로필수정기능추가

---
+++ src/main/java/com/korea/project2_team4/Model/Form/ProfileForm.java
@@ -0,0 +1,11 @@
+package com.korea.project2_team4.Model.Form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileForm {
+    private String profileName;
+    private String content;
+}

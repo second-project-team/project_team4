a56ca38133e8f8a12b7fb56cf706eb5a8b0e5775
새로ìpogeum <youyoung2774@gmail.com>
2023-12-19 13:59:08 +0900

    보관:

--- src/main/java/com/korea/project2_team4/Controller/CommunityController.java
+++ src/main/java/com/korea/project2_team4/Controller/CommunityController.java
@@ -2,6 +2,7 @@ package com.korea.project2_team4.Controller;
 
 import lombok.Builder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,4 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/community")
 public class CommunityController {
 
+    @GetMapping("/main")
+    public String main() {
+
+        return "community_main";
+    }
 }
